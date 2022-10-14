70368a661c5e5aca18c7c8dc47405b10ba14ad34
AbishekBista <abishekbista84@gmail.com>
2022-10-14 15:38:03 +0530

    First commit

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,5 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS =
+  "0xF7336089f139C63a4aF7d5937DCFd39fCb8b3E49";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
